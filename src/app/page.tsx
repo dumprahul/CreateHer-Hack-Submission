a7184da88eb491c5ce976ac9b73a586d2c828f39
dumprahul <shanrahul04@gmail.com>
2025-03-03 12:47:58 +0530

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,12 @@
 
 import { motion } from "framer-motion";
 import React from "react";
+import { useState } from "react"
 import { AuroraBackground } from "./components/aurora-background";
+import { startVerification } from "./components/startverification";
 
 export default function AuroraBackgroundDemo() {
+  const [result, setResult] = useState<any>()
   return (
     <AuroraBackground>
       <motion.div
@@ -17,15 +20,17 @@ export default function AuroraBackgroundDemo() {
         }}
         className="relative flex flex-col gap-4 items-center justify-center px-4"
       >
-        <div className="text-3xl md:text-7xl font-bold dark:text-white text-center">
-          Background lights are cool you know.
+        <div className="text-3xl md:text-7xl font-bold dark:text-white text-center text-white">
+          verify your identity through zk.
         </div>
-        <div className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4">
-          And this, is chemical burn.
+        <div className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4 text-white">
+          Click verify to get verified without revealing your details.
         </div>
-        <button className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2">
-          Debug now
+        
+        <button className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2" onClick={() => startVerification(setResult)} >
+          Verify your X 
         </button>
+        {result && <pre>{JSON.stringify(result, null, 2)}</pre>}
       </motion.div>
     </AuroraBackground>
   );
