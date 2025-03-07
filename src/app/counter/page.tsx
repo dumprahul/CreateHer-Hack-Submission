f0b809abbb966bda497931148b4c5eaa52a1c39b
dumprahul <shanrahul04@gmail.com>
2025-03-07 16:39:51 +0530

    icp-js

---
+++ src/app/counter/page.tsx
@@ -0,0 +1,9 @@
+import Counter from "../components/counter";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <Counter />
+    </div>
+  );
+}
--- src/utils/icp.js
+++ src/utils/icp.js
@@ -1,10 +1,9 @@
 import { Actor, HttpAgent } from "@dfinity/agent";
-import { idlFactory } from "../../canisters/src/declarations/backend/backend.did"; // Import auto-generated DID file
-// ../../.dfx/local/canisters/backend/backend.did.js
+import { idlFactory } from "../../canisters/src/declarations/backend/backend.did.js"; 
 
 const CANISTER_ID = "b77ix-eeaaa-aaaaa-qaada-cai"; // Replace with your actual Canister ID
 
-// Create the agent using the new method
+// Create the agent
 const agent = await HttpAgent.create({ host: "http://127.0.0.1:4943" });
 
 // Create the actor
